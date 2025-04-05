a9c42445d1bf7e3b20536da7b935f979df5a8273
Albert Weichselbraun <albert@weichselbraun.net>
2025-04-05 21:27:03 +0200

    fix: port range.

--- scripts/stop-class-instances.py
+++ scripts/stop-class-instances.py
@@ -3,5 +3,5 @@ from code_server.util.podman import start_code_server, stop_code_server
 
 CONFIG = load_config()
 
-for port in range(CONFIG.ports.c.start, CONFIG.ports.c.stop + 1):
+for port in range(CONFIG['ports']['c']['start'], CONFIG['ports']['c']['stop'] + 1):
     stop_code_server(port)
