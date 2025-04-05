2902953948d97c4b4bab3ff98a216d37cdb2b48d
Albert Weichselbraun <albert@weichselbraun.net>
2025-04-06 00:23:26 +0200

    chg: provide keys for all instances.

--- scripts/compute_authorization_key.py
+++ scripts/compute_authorization_key.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 
-from vsc_controller.util.authorize import get_authorization_key
-from vsc_controller.util.config import load_config
+from code_server.util.authorize import get_authorization_key
+from code_server.util.config import load_config
 
 CONFIG = load_config()
 
 
 course_id = input("Moodle course ID to authorized:")
-print(
-    "Authorization Code: "
-    + get_authorization_key(CONFIG["security"]["secret"], course_id)
-)
+for instance_type in CONFIG["security"]["secret"].keys():
+    print(
+        f"Authorization Code for type {instance_type}: "
+        + get_authorization_key(CONFIG["security"]["secret"][instance_type], course_id)
+    )
