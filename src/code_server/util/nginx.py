93ffac37c5e54f3fc75df33382da9bd5e15f70fe
Albert Weichselbraun <albert@weichselbraun.net>
2026-03-12 20:15:01 +0100

    fix: do not use proxy path for redirect.

--- src/code_server/util/nginx.py
+++ src/code_server/util/nginx.py
@@ -10,4 +10,4 @@ EXTRACT_RE = re.compile(
 
 def nginx_config_parser(config_file: Path) -> Dict[int, str]:
     config = config_file.read_text()
-    return {int(port): path for path, port in EXTRACT_RE.findall(config)}
+    return {int(port): path for path, port in EXTRACT_RE.findall(config) if '/proxy/' not in path}
